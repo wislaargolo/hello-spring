4d3101e870daf3e21db4d936f5177f6bd9182b3b
wislaargolo <wislaargolo1@gmail.com>
2024-03-26 22:39:51 -0300

    mostranfo contagem em hello springmvc

--- src/main/java/br/com/ufrn/helloworld/controller/HelloMVCController.java
+++ src/main/java/br/com/ufrn/helloworld/controller/HelloMVCController.java
@@ -1,5 +1,6 @@
 package br.com.ufrn.helloworld.controller;
 
+import br.com.ufrn.helloworld.dto.Greeting;
 import br.com.ufrn.helloworld.negocio.HelloNegocio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,15 +8,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Controller
 public class HelloMVCController {
 
     @Autowired
     private HelloNegocio negocio;
 
+    private final AtomicLong counter = new AtomicLong();
+
     @GetMapping("/hellomvc")
     public String greeting(@RequestParam(name="name", required = false, defaultValue="World") String name, Model model) {
-        model.addAttribute("message", negocio.message(name));
+        Greeting greeting = new Greeting(counter.incrementAndGet(), negocio.message(name));
+        model.addAttribute("greeting", greeting);
         return "hello";
     }
 
